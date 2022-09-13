c81733b105454cb576e803dda518fd823c2d4b16
M. R. Ramirez <48801344+ProfRamirez@users.noreply.github.com>
2022-09-13 14:43:42 -0300

    Exemplo 3: Herança e Ponteiros

---
+++ Exemplos/teste003/main.cpp
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------
+ * UFRJ - Universidade Federal do Rio de Janeiro
+ * IM   - Instituto de Matemática
+ * DMA  - Departamento de Matemática Aplicada
+ *
+ *   MAE015 - Tópicos em Engenharia de Dados A
+ *            "Estruturas de Dados (e seus algoritmos)"
+ *
+ *   Semana 03  : Introdução a linguagem C/C++
+ *   Exemplo 003: Herança, Hierarquia de Classes, Ponteiros e Iteradores
+ *
+ *   Autor: Prof. Milton R Ramirez (milton@matematica.ufrj.br)
+ *   Data criação  : 13 de setembro de 2022
+ *   Data alteração: 13 de setembro de 2022
+ *
+ *--------------------------------------------------------------
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "+---------------------------------------------------------------------+" << endl;
+    cout << "| Exemplo 003: Herança, Hierarquia de Classes, Ponteiros e Iteradores |" << endl;
+    cout << "+---------------------------------------------------------------------+" << endl << endl;
+
+    return 0;
+}
